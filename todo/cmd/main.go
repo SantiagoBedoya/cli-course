6194378a2a42827288acdc52e4c6ebbc20391859
SantiagoBedoya <a.sbedoya200110@gmail.com>
2023-02-19 09:20:15 -0500

    adding todo cli

--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	"github.com/SantiagoBedoya/cli-course/todo"
 )
 
-const todoFileName = ".todo.json"
+var todoFileName = ".todo.json"
 
 func main() {
+
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
+
+	flag.Usage = func() {
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+
+	add := flag.Bool("add", false, "Add Task to the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	del := flag.Int("del", 0, "Item to be deleted")
+	verbose := flag.Bool("v", false, "Verbose mode")
+
+	flag.Parse()
+
 	l := &todo.List{}
 
 	if err := l.Get(todoFileName); err != nil {
@@ -19,17 +40,62 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
-		for _, item := range *l {
-			fmt.Println(item.Task)
+	case *list:
+		if *verbose {
+			fmt.Print(l.PrintVerbose())
+			return
 		}
-
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
+		fmt.Print(l)
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case *del > 0:
+		if err := l.Delete(*del); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
